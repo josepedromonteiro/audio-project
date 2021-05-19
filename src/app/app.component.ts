b20d2e498f54c2868ec5eadb754c89136bad5e35
Pedro Monteiro <josepedromonteiro@outlook.com>
2021-05-19 22:34:14 +0100

    Added speech to text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import Speech from 'speak-tts';
+import {LanguageService} from './services/languages/language.service';
+import {SpeakService} from './services/speak/speak.service';
 
-
-export interface Lang {
-  code: string;
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -15,44 +11,19 @@ export interface Lang {
 })
 export class AppComponent implements OnInit {
   title = 'audio-project';
-  public speech: Speech;
   public audioSource: string;
-  public lang: Lang;
-  public langs: Lang[] = [];
   public activeLanguage: string;
   public text: string;
 
+  constructor(public languageService: LanguageService,
+              public speakService: SpeakService) {
+  }
+
+
   ngOnInit(): void {
-    this.speech = new Speech();
-    this.speech.init({
-      volume: 1,
-      lang: 'en-GB',
-      rate: 1,
-      pitch: 1,
-      splitSentences: true,
-      listeners: {
-        onvoiceschanged: (voices) => {
-          console.log('Event voiceschanged', voices);
-        }
-      }
-    });
 
-    if (this.speech.hasBrowserSupport()) {
-      console.log('speech synthesis supported');
-    }
 
-    this.langs = [
-      {
-        name: 'Português',
-        code: 'pt-PT'
-      },
-      {
-        name: 'English',
-        code: 'en-GB'
-      }
-    ];
-
-    this.activeLanguage = this.langs[0].code;
+    this.activeLanguage = this.languageService.langs[0].code;
   }
 
   playAudio(): void {
@@ -67,8 +38,8 @@ export class AppComponent implements OnInit {
   }
 
   public textToSpeech(): void {
-    this.speech.setLanguage(this.activeLanguage);
-    this.speech.speak({
+    this.speakService.speech.setLanguage(this.activeLanguage);
+    this.speakService.speech.speak({
       text: this.text,
       queue: false
     });
